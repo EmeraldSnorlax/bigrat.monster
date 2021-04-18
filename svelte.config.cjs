fadf0067f23070008c7ac46444d08a00e24d03d9
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-04-18 13:48:25 +0100

    not shit and cum and die if we are on github pages

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -2,6 +2,16 @@ const sveltePreprocess = require('svelte-preprocess')
 const ssr = require('@sveltejs/adapter-static')
 const pkg = require('./package.json')
 
+const fs = require('fs')
+
+let base;
+
+if (fs.existsSync('./CNAME') || process.env.NODE_ENV === 'development') {
+  base = ''
+} else {
+  base = '/bigrat.monster'
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
   preprocess: [
@@ -19,6 +29,10 @@ module.exports = {
       ssr: {
         noExternal: Object.keys(pkg.dependencies || {})
       }
+    },
+    paths: {
+      assets: base,
+      base: base,
     }
   }
 }
