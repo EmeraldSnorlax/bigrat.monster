24724006c58a357f777a60e9177cd9a6778233f6
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-04-18 13:13:50 +0100

    Initial commit, bootstrapped
    https://github.com/dansvel/sveltekit-typescript-postcss-tailwind2

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import '../global.postcss'
+  import { theme } from './store'
+  import ToggleTheme from '../lib/ToggleTheme.svelte';
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    if ($theme === 'dark') {
+      document.querySelector('html').classList.add('dark')
+    }
+  })
+</script>
+
+<main>
+  <ToggleTheme />
+  <slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { theme } from './store'
+  import ToggleTheme from '$lib/ToggleTheme.svelte'
+</script>
+
+<svelte:head>
+  <title>bigrat | home</title>
+</svelte:head>
+
+<h1 class="text-5xl font-extrabold mb-2">Hello world!</h1>
+<h2 class="text-3xl font-bold mb-4">Now using SvelteKit beta</h2>
+
+
+
