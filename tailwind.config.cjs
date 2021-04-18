ef72db4edfcf1c911dbc73b38710e19fe0301f3c
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-04-18 15:12:53 +0100

    colours

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,19 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        bigrat: 'url(\'bigrat.png\')'
+        'bigrat-img': 'url(\'bigrat.png\')'
+      },
+      colors: {
+        error: { DEFAULT: '#d6351c' },
+        'true-gray': { 
+          dark: '#121212',
+          DEFAULT: '#212121',
+          bigrat: '#4b4c51',
+          light: '#313131',
+          lighter: '#424242',
+          lightest: '#515151',
+        },
       }
-    }
+    },
   }
 }
